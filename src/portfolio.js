545364caa9313571a0ab465bc32d4dd76f14508b
shit-happens <avishwakarma1@me.iitr.ac.in>
2024-07-05 07:17:28 +0530

    change in content

--- src/portfolio.js
+++ src/portfolio.js
@@ -293,6 +293,20 @@ const achievementSection = {
     {
       title: "Spot Award - Landmark Group",
       subtitle:
+        "Awarded for exemplary contributions to the team in Q4 of FY 23-24",
+      image: require("./assets/images/LANDMARK-OG-TAG-LOGO.jpg"),
+      imageAlt: "PWA Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://drive.google.com/file/d/19dp_20ewZ9XOrToW7euIRhWzJiZBHR1o/view?usp=drive_link"
+        }
+      ]
+    },
+
+    {
+      title: "Spot Award - Landmark Group",
+      subtitle:
         "Awarded for exemplary contributions to the team in Q4 of FY 22-23",
       image: require("./assets/images/LANDMARK-OG-TAG-LOGO.jpg"),
       imageAlt: "PWA Logo",
@@ -358,16 +372,16 @@ const blogSection = {
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://medium.com/@anshitvishwa111/hosting-machine-learning-models-as-an-api-service-1cf5cb5a1e2f",
+      title: "Hosting Machine Learning Models as an API Service",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+        "How I have been deploying my ML models?"
     },
     {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
+      url: "https://medium.com/@anshitvishwa111/fashions-secret-weapon-unleashing-data-science-in-the-apparel-retail-industry-b21a915a9a4c",
+      title: "Fashion’s Secret Weapon: Unleashing Data Science in the Apparel Retail Industry",
       description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+        "Deep dive into role of data scientists in markdown and assortment planning"
     }
   ],
   display: true // Set false to hide this section, defaults to true
